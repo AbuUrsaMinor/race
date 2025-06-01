4454914ca147a9a50ca534ee0ac1c5a9cbbd592c
Erik Johansson <erik.b.johansson@afconsult.com>
2025-06-01 11:04:37 +0200

    reinitialcommit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,19 +1,1 @@
 /// <reference types="vite/client" />
-
-// Add Screen Orientation API definitions
-interface ScreenOrientation {
-    angle: number;
-    type: string;
-    onchange: ((this: ScreenOrientation, ev: Event) => any) | null;
-    lock(orientation: string): Promise<void>;
-    unlock(): void;
-}
-
-interface Screen {
-    orientation: ScreenOrientation;
-}
-
-// Add WebKit Audio Context definitions
-interface Window {
-    webkitAudioContext: typeof AudioContext;
-}
